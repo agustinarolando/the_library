7f12e82ded8d79f672c96568aed54fafae684756
Agustina Rolando <agustinarolando@gmail.com>
2023-12-18 17:24:57 -0300

    replaced files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    document.getElementById("indexButton").addEventListener("click", ()=>{
+        window.location.href="books.html"
+    });
+
+});
